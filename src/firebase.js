4539a8d8f8a155dc2cc410cc52e773c539609b5c
yamathe <manuelsegmar@gmail.com>
2022-08-09 15:25:57 -0500

    ADD: APIKEY

--- src/firebase.js
+++ src/firebase.js
@@ -13,12 +13,18 @@ import { getFirestore } from "firebase/firestore"
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyDLX3qRJSGpaV42jSbhWZABj8lSQ4Y636Q",
+  authDomain: "todo-app-c68bd.firebaseapp.com",
+  projectId: "todo-app-c68bd",
+  storageBucket: "todo-app-c68bd.appspot.com",
+  messagingSenderId: "308061022726",
+  appId: "1:308061022726:web:54a9ce1193cb682a23b187"
 };
 
 // Initialize Firebase
