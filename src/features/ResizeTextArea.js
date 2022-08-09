4f90013e5a1981d775373191933eb5daff958f6f
yamathe <manuelsegmar@gmail.com>
2022-08-09 14:17:00 -0500

    ADD: function for resize textarea

---
+++ src/features/ResizeTextArea.js
@@ -0,0 +1,8 @@
+
+export default function ResizeTextArea(element, defaultHeight) {
+  if(element){
+    const target = element.target ? element.target : element
+    target.style.height = defaultHeight
+    target.style.height = `${target.scrollHeight}px`
+  }
+}
