4cccf38832c02c9434bf7e2399a67e6066e04f64
yamathe <manuelsegmar@gmail.com>
2022-08-08 19:10:38 -0500

    ADD: Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className='header__container'>
+        <h2>Comunity Page</h2>
+        <h2>My Page</h2>
+      </div>
+    </div>
+  )
+}
