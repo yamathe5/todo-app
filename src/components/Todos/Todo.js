b9f77f1e7bbdca8f4996bc2f1e507618646838db
yamathe <manuelsegmar@gmail.com>
2022-08-09 14:14:56 -0500

    CHORE: pass props fetch delete function

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,11 +1,20 @@
+import {ReactComponent as TrashIcon} from "../../svg/trash-icon.svg"
 
-export default function Todo ({todo, dispatch}){
+export default function Todo ({todo, dispatch, deleteTodo}){
+
+  function handleDelete(){
+    dispatch({type:"delete-todo", payload:{id:todo.id}})
+    deleteTodo(todo.id)
+  }
+
   return (
   <div className="todo">
     <h3 className="todo__title" >{todo.name}</h3>
     <p className="todo__content">{todo.content}</p>
+    
+    
     {/* <button className="todo__complete" onClick={() => dispatch({type:"complete-todo", payload:{id:todo.id}})}>Completed</button> */}
-    <button className="todo__delete" onClick={() => dispatch({type:"delete-todo", payload:{id:todo.id}})}>Delete</button>
+    <button className="todo__delete" onClick={handleDelete}><TrashIcon/></button>
   </div>
   )
 }
